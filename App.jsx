5f3082f209dcbce06a464e4c8ecb7156ab899e0c
Sakshi160205 <sakshibk1602@gmail.com>
2025-01-02 23:53:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+import Navbar from './assets/Components/Navbar'
+import Home from './assets/Components/Home'
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      
+    </div>
+  );
+};
+
+export default App;
