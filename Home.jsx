b1c6d5e1882a20062693ef179881f4d3a3f8a875
Sakshi160205 <sakshibk1602@gmail.com>
2025-01-02 23:53:58 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to My React Website</h1>
+      <p>This is a simple React-based website!</p>
+    </div>
+  );
+};
+
+export default Home;
